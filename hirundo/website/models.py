60b3c509b9fb0ddf51fa7597b23797dfbbe04baf
vasil <vdtodorov93@gmail.com>
2014-05-25 14:27:49 +0300

    Fixing the admin pannel issue

--- hirundo/website/models.py
+++ hirundo/website/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
     text = models.TextField(blank=False)
     pub_date = models.DateTimeField(auto_now=True)
     location = models.TextField(blank=True)
+    author = models.ForeignKey(User)
+
--- hirundo/website/urls.py
+++ hirundo/website/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'home', name="home"),
-    url(r'^users', 'users', name="users"),
-    url(r'^messages', 'messages', name="messages"),
+    #url(r'^users', 'users', name="users"),
+    #url(r'^messages', 'messages', name="messages"),
 )
