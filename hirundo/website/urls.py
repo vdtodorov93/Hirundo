073c2f6235d136035ab44a1ba164bf0704598118
vasil <vdtodorov93@gmail.com>
2014-05-25 15:07:52 +0300

    started making the register form

--- hirundo/website/urls.py
+++ hirundo/website/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'home', name="home"),
+    url(r'^register/$', 'register', name='register')
     #url(r'^users', 'users', name="users"),
     #url(r'^messages', 'messages', name="messages"),
 )
--- hirundo/website/views.py
+++ hirundo/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from models import Message
+from django.contrib.auth.models import User
 
 def home(request):
     a = 5
     return render(request, "home.html", locals())
 
+def register(request):
 
 
 
