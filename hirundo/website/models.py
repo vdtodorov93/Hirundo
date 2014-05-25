3d0ec2ab4935e2fad9439bbcde49034d638961cb
vasil <vdtodorov93@gmail.com>
2014-05-25 13:35:02 +0300

    Adding the message model

--- hirundo/website/models.py
+++ hirundo/website/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    text = models.TextField(blank=False)
+    pub_date = models.DateTimeField(auto_now=True)
+    location = models.TextField(blank=True)
